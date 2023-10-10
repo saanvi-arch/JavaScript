c4635407ce7598d5c9ddd3c0feaa48c313af5f65
saanvi-arch <69002236+saanvi-arch@users.noreply.github.com>
2023-10-10 12:32:02 +0000

    container config done

---
+++ lecture1/variable.js
@@ -0,0 +1,18 @@
+const accountId = 14453
+let accountEmail = "saanvi@google.com"
+var accPassword ="12345"
+accountCity = "Jaipur"
+let accountState;
+
+// accountId = 2 // cannot be changed
+
+accountEmail = "sita@hc.com"
+accountCity = "Pune"
+accPassword="5151515"
+
+console.table([accountId , accPassword , accountCity , accountEmail , accountState])
+
+/*
+Do not use Var becoz of issue in block scope and functional scope
+*/
+
