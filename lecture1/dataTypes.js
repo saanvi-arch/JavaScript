6daed5bdd276882247c460b5dd0074d4d8d33bc1
saanvi-arch <69002236+saanvi-arch@users.noreply.github.com>
2023-10-10 12:57:17 +0000

    container config done

---
+++ lecture1/dataTypes.js
@@ -0,0 +1,23 @@
+"use strict " // treate all js code as newer version 
+
+// alert(3+3) // we are using node js not browser 
+
+console.log( 3 + 3)
+
+console.log("saanvi ")
+
+let name ="saanvi"
+let age = 22
+
+console.log(typeof "saanvi")
+console.log(typeof undefined)
+console.log(typeof age)
+console.log(typeof null)
+
+// number => 2^53
+// string
+// bigint
+// boolean
+// null : standalone value
+// undefined : we declared the variable but didnt assign any value to that variable 
+// symbol : used for uniqueness
